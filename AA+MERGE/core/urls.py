0039efe513a37b8cd2b03c050e16a6b78fa32845
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-09-17 17:04:40 +0545

    Added UI , rendered data

--- AA+MERGE/core/urls.py
+++ AA+MERGE/core/urls.py
@@ -1,11 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
+from .views import HomeView
 
 
 app_name='core'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    
    
 ]
--- AA+MERGE/core/views.py
+++ AA+MERGE/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Item
 # Create your views here.
+class HomeView(ListView):
+    model = Item
+    paginate_by = 8
+    template_name = "home.html"
