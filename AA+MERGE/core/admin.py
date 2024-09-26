e71dba77f3dcdf0ae1870b1b226daa1045d84412
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-09-26 11:10:55 +0545

    Feat:Customize Admin Pannel

--- AA+MERGE/core/admin.py
+++ AA+MERGE/core/admin.py
@@ -1,10 +1,61 @@
 from django.contrib import admin
 from .models import Item, OrderItem, Order,Payment,Coupon,Address
 # Register your models here.
+
+
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_requested=False, refund_granted=True)
+
+
+make_refund_accepted.short_description = 'Update orders to refund granted'
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'ordered',
+                    'being_delivered',
+                    'received',
+                    'refund_requested',
+                    'refund_granted',
+                    'shipping_address',
+                    'payment',
+                    'coupon'
+                    ]
+    list_display_links = [
+        'user',
+        'shipping_address',
+        'payment',
+        'coupon'
+    ]
+    list_filter = ['ordered',
+                   'being_delivered',
+                   'received',
+                   'refund_requested',
+                   'refund_granted']
+    search_fields = [
+        'user__username',
+        'ref_code'
+    ]
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'address',
+        'street_address',
+        'country',
+        'zip',
+        'phone_number',
+        'default',
+        
+    ]
+    list_filter = ['default', 'country']
+    search_fields = ['user', 'street_address', 'address', 'zip']
+
+
 admin.site.register(Item)
 admin.site.register(OrderItem)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(Coupon)
-admin.site.register(Address)
-
+admin.site.register(Address, AddressAdmin)
