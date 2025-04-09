03c1fe82e87e105088489e2b21cceecb207ca6bf
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-04-09 15:00:46 +0545

    TODO:Added new todo

---
+++ AA+MERGE/TODO.py
@@ -0,0 +1,27 @@
+# TODO:Create a payment method for the user to pay for the order use esewa ,khalti and stripe
+done= False
+
+# TODO:Integrate a Machine Learning model to predict  feedback positive or negative
+done= False
+
+# TODO:Fix some UI
+done= False
+
+# TODO:Validate user input in the form
+done= False
+
+# TODO: Create a robust error handling mechanism to handle errors and exceptions
+done= False
+
+# TODO:Create a standard and follow that  for maintainability
+done= False
+
+# TODO:Update a readme file for the project
+done= False
+
+# TODO:Create a service file for the project to handle the business logic
+done= False
+
+# TODO:Create a Secure integration with payment gateways like Stripe etc.
+done= False
+
