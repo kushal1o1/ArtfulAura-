0fffadbc406ac6ecd642e32b76f9764c970bbe05
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-04-09 16:42:36 +0545

    TODO:flow

--- TODO.py
+++ TODO.py
@@ -1,5 +1,20 @@
 # TODO:Create a payment method for the user to pay for the order use esewa ,khalti and stripe
-done= False
+done= False 
+#flow - payment  url hit either esewa or khalti,
+# for esewa  
+    # 1. get the order id and amount from the order summary page and redirect to payment.html page 
+    # 2. redirect to esewa payment page with the order id and amount as parameters from payment.html page with required fields
+    # 3. after payment is done, esewa will redirect to the verify-esewa url with the order id and amount as parameters
+    # 4. verify the payment by checking the order id and amount with the database
+    # 5. if payment is successful, update the order status to paid and redirect to the order summary page
+    # 6. if payment is not successful, redirect to the order summary page with an error message
+# for khalti
+    # 1. get the order id and amount from the order summary page
+    # 2. redirect to khalti payment page with the order id and amount as parameters
+    # 3. after payment is done, khalti will redirect to the verify-khalti url with the order id and amount as parameters
+    # 4. verify the payment by checking the order id and amount with the database
+    # 5. if payment is successful, update the order status to paid and redirect to the order summary page
+    # 6. if payment is not successful, redirect to the order summary page with an error message
 
 # TODO:Integrate a Machine Learning model to predict  feedback positive or negative
 done= False
