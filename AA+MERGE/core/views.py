93b5aef43c151c59aa5ea47f7c697c4dbbe6801e
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-04-11 22:32:03 +0545

    Feat:Created services like genrate signature,generate transactionuuid,create refcode

--- AA+MERGE/core/views.py
+++ AA+MERGE/core/views.py
@@ -19,7 +19,7 @@ from decouple import config
 import random
 import string
 import time
-from .services import add_to_cart_service,remove_from_cart_service,remove_single_item_from_cart_service
+from .services import add_to_cart_service,remove_from_cart_service,remove_single_item_from_cart_service,generate_transaction_uuid,generate_signature,create_ref_code,is_valid_form
 
 # Create your views here.
 class HomeView(ListView):
@@ -145,13 +145,6 @@ class OrderSummaryView(LoginRequiredMixin, View):
             return redirect("/")
         
         
-def is_valid_form(values):
-    valid = True
-    for field in values:
-        if field == '':
-            valid = False
-    return valid
-
 
 
 
@@ -312,56 +305,6 @@ class AddCouponView(LoginRequiredMixin,View):
         else:
             messages.info(self.request, "This coupon does not exist")
             return redirect("core:checkout")
-
-
-
-
-
-# Generate transaction UUID
-def generate_transaction_uuid():
-    # Generate a simple transaction ID in the format shown in docs: xx-xxx-xx
-    first = random.randint(10, 99)
-    middle = random.randint(100, 999)
-    last = random.randint(10, 99)
-    return f"{first}-{middle}-{last}"
-
-
-
-def generate_signature(
-        total_amount: float, 
-        transaction_uuid: str, 
-        key: str = "8gBm/:&EnhH.1/q", 
-        product_code: str = "EPAYTEST"
-) -> str:
-    """Generates hmac sha256 signature for eSewa payment gateway
-    """
-    if not total_amount or not transaction_uuid:
-        raise ValueError("Both 'total_amount' and 'transaction_uuid' are required.")
-    try:
-        message = f"total_amount={total_amount},transaction_uuid={transaction_uuid},product_code={product_code}"
-        key = key.encode('utf-8')
-        message = message.encode('utf-8')
-
-        # Generate HMAC-SHA256 digest
-        hmac_sha256 = hmac.new(key, message, hashlib.sha256)
-        digest = hmac_sha256.digest()
-
-        # Convert to Base64
-        signature = base64.b64encode(digest).decode('utf-8')
-        return signature
-    except Exception as e:
-        raise RuntimeError(f"Failed to generate signature: {e}")
-
-def generate_transaction_uuid():
-    current_time = int(time.time())
-    first = random.randint(10, 99)
-    middle = random.randint(100, 999)
-    last = current_time % 100  
-    return f"{first}-{middle}-{last}"
-
-
-def create_ref_code():
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=20))
 
 
 class PaymentView(LoginRequiredMixin,View):
