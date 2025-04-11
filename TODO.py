7ce228ce2e1df5b19f0aaac5f4ccbf21d7966d32
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-04-11 21:43:16 +0545

    TODO:completed Working of Esewa

--- TODO.py
+++ TODO.py
@@ -1,11 +1,11 @@
 # TODO:Create a payment method for the user to pay for the order use esewa ,khalti and stripe
-done= False 
+done= True 
 #flow - payment  url hit either esewa or khalti,
 # for esewa  
-True    # 1. get the order id and amount from the order summary page and redirect to payment.html page  
+    # 1. get the order id and amount from the order summary page and redirect to payment.html page  
 
-True  # 2. redirect to esewa payment page with the order id and amount as parameters from payment.html page with required fields
-True    # 3. after payment is done, esewa will redirect to the verify-esewa url with the order id and amount as parameters
+    # 2. redirect to esewa payment page with the order id and amount as parameters from payment.html page with required fields
+    # 3. after payment is done, esewa will redirect to the verify-esewa url with the order id and amount as parameters
     # 4. verify the payment by checking the order id and amount with the database
     # 5. if payment is successful, update the order status to paid and redirect to the order summary page
     # 6. if payment is not successful, redirect to the order summary page with an error message
